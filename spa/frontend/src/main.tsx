bac1246fcf17de3e02a3f0670cf727d78a097792
hyh8414 <hyh8414@gmail.com>
2025-10-18 11:40:01 +0900

    capacitor 의존성 추가

--- spa/frontend/src/main.tsx
+++ spa/frontend/src/main.tsx
@@ -6,6 +6,16 @@ import Home from './pages/Home'
 import Todos from './pages/Todos'
 import Settings from './pages/Settings'
 
+import { StatusBar, Style } from '@capacitor/status-bar';
+
+(async () => {
+  // 상태바를 WebView 위에 ‘겹치지 않게’ (= WebView를 아래로 밀기)
+  await StatusBar.setOverlaysWebView({ overlay: false });
+
+  // 다크 배경이면 아이콘 밝게
+  await StatusBar.setStyle({ style: Style.Dark });
+})();
+
 const router = createBrowserRouter([
   {
     path: '/',
