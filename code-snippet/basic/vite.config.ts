4667f2053b530200f068648c2b44fc6a15dad698
함영현 <hyh8414@gmail.com>
2025-10-22 17:59:06 +0900

    vercel 설정 수정

--- code-snippet/basic/vite.config.ts
+++ code-snippet/basic/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    headers: {
+      // OAuth 팝업과의 통신 허용
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+      // COEP를 강제하지 않음
+      'Cross-Origin-Embedder-Policy': 'unsafe-none',
+    },
+  },
+});
