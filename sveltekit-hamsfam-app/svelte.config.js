234ffb2b9ffcb7f6ad6674ddbf52716269bafb91
hyeon 함영현(Young-Hyun HAM) <9025014@clt.net>
2026-01-20 14:19:06 +0900

    svelte.config adapter vercel 로 변경 처리 함

--- sveltekit-hamsfam-app/svelte.config.js
+++ sveltekit-hamsfam-app/svelte.config.js
@@ -1,21 +1,32 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const config = {
+export default {
   preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter(
-      // {
-      // // SPA 모드: 어떤 라우트든 index.html로 fallback
-      // fallback: 'index.html'
-      // }
-    ),
-    prerender: {
-      // ✅ 모든 페이지를 미리 HTML로 뽑지 않겠다 = SPA처럼 동작
-      entries: []
-    }
+    adapter: adapter()
   }
 };
 
-export default config;
+// 정적 사이트(SPA)
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// const config = {
+//   preprocess: vitePreprocess(),
+
+//   kit: {
+//     adapter: adapter(
+//       // {
+//       // // SPA 모드: 어떤 라우트든 index.html로 fallback
+//       // fallback: 'index.html'
+//       // }
+//     ),
+//     prerender: {
+//       // ✅ 모든 페이지를 미리 HTML로 뽑지 않겠다 = SPA처럼 동작
+//       entries: []
+//     }
+//   }
+// };
+
+// export default config;
